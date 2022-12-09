c0c6c4884efd9ff5ad64f663fad5c89807cd9d42
monosniper <vanteateam@gmail.com>
2022-12-09 23:55:03 +0500

    sad

--- src/App.js
+++ src/App.js
@@ -4,22 +4,13 @@ import './scss/App.scss';
 import Footer from "./components/layout/Footer";
 import "../node_modules/noty/lib/noty.css";
 import "../node_modules/noty/lib/themes/mint.css";
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
-
-const initialOptions = {
-    "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID,
-    currency: "RUB",
-    intent: "capture",
-};
 
 function App() {
     return (
-        <PayPalScriptProvider options={initialOptions}>
-            <BrowserRouter>
-                <AppRouter/>
-                <Footer />
-            </BrowserRouter>
-        </PayPalScriptProvider>
+        <BrowserRouter>
+            <AppRouter/>
+            <Footer />
+        </BrowserRouter>
     );
 }
 
